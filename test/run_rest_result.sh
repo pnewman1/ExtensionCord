728352ed50187cf83d59d6440d3eee1a9467d96e
patorick1@gmail.com <patorick1@gmail.com>
2015-04-28 10:35:29 -0700

    Sample change for pull request

--- test/run_rest_result.sh
+++ test/run_rest_result.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#this is a sample change for demonstration
 python ../extension_cord/static/scripts/InputTestResultsInEC.py rest_result_01.json http://127.0.0.1:8000/api/result
 python ../extension_cord/static/scripts/InputTestResultsInEC.py rest_result_02.json http://127.0.0.1:8000/api/result
